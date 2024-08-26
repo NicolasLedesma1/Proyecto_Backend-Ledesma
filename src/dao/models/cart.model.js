f3599ef81a159026b33f5022ac18ef59a87357c8
nicolas ledesma <n.ledesma0194@gmail.com>
2024-08-26 14:47:43 -0300

    mongooseIndex

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -4,15 +4,20 @@ const cartSchema = new mongoose.Schema({
     products: [
         {
             product: {
-                type: String, 
-                require: true
-            }, 
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "productos" 
+            },
             quantity: {
                 type: Number, 
                 required: true
             }
         }
     ]
+})
+
+cartSchema.pre("findOne", function(next){
+    this.populate("products.product");
+    next();
 })
 
 const CartModel = mongoose.model("carts", cartSchema);
--- src/db.js
+++ src/db.js
@@ -1,14 +1,16 @@
 const mongoose = require ("mongoose");
 const ProductModel = require("./dao/models/productos.models");
+const CartModel = require("./dao/models/cart.model");
 // conexion con mongoose
 
 const main = async () =>{
     await mongoose.connect("mongodb+srv://nledesma0194:camilo2019@codercluster.jsfp4.mongodb.net/Supermercado?retryWrites=true&w=majority&appName=coderCluster")
     .then(()=> console.log("tenemos la bd"))
     .catch(() => console.log("no tenemos la bd"))
-    const respuesta = await ProductModel.find({title : "Mate"}).explain("executionStats");
-
-    console.log(respuesta);
+    
+    const carritoConProducto = await CartModel.findById("66ca34cf7778d1f19cf6bbe3")
+    console.log(JSON.stringify(carritoConProducto, null, 2));
+    
     
     }
 
